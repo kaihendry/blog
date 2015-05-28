c08dcc08d956fb28bc17421df16b3eb9e1860e2f
Kai Hendry <hendry@webconverger.com>
2015-05-28 20:58:17 +0800

    A go package

---
+++ meta.go
@@ -0,0 +1,60 @@
+package blog
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func parseMetaLine(line string) (string, string, error) {
+	item := strings.TrimPrefix(line, `[[!meta `)
+	splitItem := strings.Split(item, "=\"")
+	fmt.Println(splitItem)
+	splitItem[1] = strings.TrimSuffix(splitItem[1], "\" ]]")
+	fmt.Println(splitItem[1])
+	if len(splitItem) != 2 {
+		return "", "", fmt.Errorf("Error parsing line")
+	}
+	return splitItem[0], splitItem[1], nil
+}
+
+func GetKey(fileName string, keys ...string) map[string]string {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	result := map[string]string{}
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return result
+		}
+
+		if !strings.HasPrefix(line, "[[!meta") {
+			break
+		}
+
+		key, value, err := parseMetaLine(line)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		for _, k := range keys {
+			if key == k {
+				result[key] = value
+			}
+		}
+
+	}
+	return result
+}
