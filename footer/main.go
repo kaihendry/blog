471f6cd8a03f1bf6a9614c4177e0a37408cd80e3
Kai Hendry <hendry@webconverger.com>
2015-09-27 14:38:07 +0800

    Trying to piggy back in fugly Google Analytics to get errors back to me

--- footer/main.go
+++ footer/main.go
@@ -27,9 +27,33 @@ func main() {
 	t, err := template.New("foo").Parse(`<footer><p><a href=https://github.com/kaihendry/natalian/blob/mk/{{ .Mdwn }}>Source</a></p>
 	<p><a href="mailto:hendry+blog@iki.fi?subject=natalian.org/{{ .URL }}%20feedback">Email me feedback please!</a></p>
 	<p><a href="https://validator.nu/?doc=http://natalian.org/{{ .URL }}">Validate me</a></p>
-	<p><a href="https://validator.w3.org/checklink?uri=http://natalian.org/{{ .URL }}&amp;hide_type=all&amp;depth=&amp;check=Check">Check links</a></p>
 	</footer>
 	</article>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-195686-1', 'auto');
+  ga('send', 'pageview');
+
+function trackJavaScriptError(e) {
+        var ie = window.event || {},
+            errMsg = e.message || ie.errorMessage;
+        var errSrc = (e.filename || ie.errorUrl) + ': ' + (e.lineno || ie.errorLine);
+        ga('send', 'event', 'JavaScript Error', errMsg, errSrc, { 'nonInteraction': 1 });
+}
+
+if (window.addEventListener) {
+  window.addEventListener('error', trackJavaScriptError, false);
+} else if (window.attachEvent) {
+  window.attachEvent('onerror', trackJavaScriptError);
+} else {
+  window.onerror = trackJavaScriptError;
+}
+</script>
 	</body>
 	</html>
 `)
